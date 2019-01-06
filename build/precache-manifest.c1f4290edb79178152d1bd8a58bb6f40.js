fdf2a29b5b098d8ffe23e45e14690e5a3eb52091
Leyla Azizova <smallcrazy@Leylas-MacBook-Pro.local>
2019-01-05 20:45:26 -0800

    initial commit

---
+++ build/precache-manifest.c1f4290edb79178152d1bd8a58bb6f40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f598391d168466efb222",
+    "url": "/static/js/main.f598391d.chunk.js"
+  },
+  {
+    "revision": "b92ceea54def071ed595",
+    "url": "/static/js/1.b92ceea5.chunk.js"
+  },
+  {
+    "revision": "f598391d168466efb222",
+    "url": "/static/css/main.b27c9dab.chunk.css"
+  },
+  {
+    "revision": "89733f806eb0b352f521b42871999d2e",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,131 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import simulation from './Simulation.js'
+import React, { Component } from 'react';
 
-class App extends Component {
+class Unit extends React.Component {
+  handleInputChange = (event) => {
+    const { value, name } = event.currentTarget;
+    const newStats = {
+      ...this.props.values,
+      [name]: value
+    };
+
+    this.props.onChange(this.props.unitKey, newStats);
+  };
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <div>{this.props.unitKey}</div>
+        {this.renderInputs()}
+      </div>
+    );
+  }
+
+  renderInputs = () => {
+    return Object.keys(this.props.values).map(this.renderInput);
+  };
+
+  renderInput = (statName) => {
+    return (
+      <div key={statName}>
+        <label>
+          {statName}{' '}
+          <input
+            type="text"
+            value={this.props.values[statName]}
+            name={statName}
+            onChange={this.handleInputChange}
+          />
+        </label>
+      </div>
+    );
+  };
+}
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      results: {
+        turns: 0,
+        winner: 'press button to start simulation',
+      },
+      unit1: {
+        name: 'Unit 1',
+        green: 2,
+        yellow: 4,
+        red: 4,
+        dice: 5,
+        attack: 4,
+        power: 3,
+        defense: 2,
+        toughness: 1,
+        courage: 12,
+        range: 0,
+        speed: 3.5,
+        fear_level: 1,
+        impact_hits: 0
+      },
+      unit2: {
+        name: 'Unit 2',
+        green: 2,
+        yellow: 4,
+        red: 4,
+        dice: 5,
+        attack: 4,
+        power: 3,
+        defense: 2,
+        toughness: 1,
+        courage: 12,
+        range: 0,
+        speed: 3.5,
+        fear_level: 1,
+        impact_hits: 0
+      }
+    };
+  }
+
+  handleSimulateClick = () => {
+    const results = simulation.simulateGame(this.state.unit1, this.state.unit2);
+
+    this.setState({
+      results
+    });
+  };
+
+  handleUnitPropertyValueChange = (unitKey, newStats) => {
+    this.setState({
+      [unitKey]: newStats
+    });
+  };
+
+  render() {
+    console.log(this.state.results);
+    return (
+      <div className = 'App'>
+        <header className = 'App-header'>
+          <h1>Battle simulator</h1>
+          <div>{this.renderUnit(this.state.unit1, 'unit1')}</div>
+          <div>{this.renderUnit(this.state.unit2, 'unit2')}</div>
+          <button onClick={this.handleSimulateClick}>Simulate</button>
+          <h2>Simulation results</h2>
+          <div>Turns: {this.state.results.turns}</div>
+          <div> Winner: {this.state.results.winner}</div>
+          <div>Loser order: {this.state.results.loser_order} </div>
         </header>
       </div>
     );
   }
+
+  renderUnit = (stats, unitKey) => {
+    return <Unit
+      values={stats}
+      unitKey={unitKey}
+      onChange={this.handleUnitPropertyValueChange}
+    />;
+  };
 }
 
 export default App;
